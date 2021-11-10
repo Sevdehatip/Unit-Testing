4721b82838e04ca0b6c4fcd26b63239ad65ecc30
Zehra Sevde Hatipoğlu <46373642+Sevdehatip@users.noreply.github.com>
2021-11-10 20:49:52 +0300

    unit test added :D

---
+++ Test_MathOperations/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_MathOperations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Test_MathOperations")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b01e881b-9424-4932-8ea7-03c8fdb43fa8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test_MathOperations/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MathOperations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_MathOperations
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        BasicOp op = new BasicOp();
+        [TestMethod]
+        public void Test_AdditionMethod()
+        {
+            double result = op.Addition(10, 10);
+            Assert.AreEqual(result, 20);
+        }
+
+        [TestMethod]
+        public void Test_MultiplicationMethod()
+        {
+            double result = op.Multiplication(10, 10);
+            Assert.AreEqual(result, 100);
+        }
+
+        [TestMethod]
+        public void Test_SubtractionMethod()
+        {
+            double result = op.Subtraction(10, 10);
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void Test_DivisionMethod()
+        {
+            double result = op.Division(10, 10);
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
